473ca74e9a41fdf886148ae0498874044b8cb533
ahmedJamaey <a.gmal@granadapride.com>
2024-12-30 20:58:14 +0200

    three commit

---
+++ database/migrations/2024_12_30_183004_create_plans_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('plans', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('duration_unit');
+            $table->text('description')->nullable();
+            $table->integer('price');
+            $table->decimal('discount_price', 8, 2);
+            $table->string('discount_type');
+            $table->timestamps();
+        });
+
+        Schema::create('features', function(Blueprint $table){
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->string('key');
+            $table->string('value');
+            $table->timestamps();
+        });
+
+        Schema::create('plan_feature', function(Blueprint $table){
+            $table->id();
+            $table->foreignId('plan_id')->constrained()->onDelete('cascade');
+            $table->foreignId('feature_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('plans');
+    }
+};
