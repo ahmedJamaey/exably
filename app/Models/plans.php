b7c452262f78383b52b8d2247cb8e28410707be4
ahmedJamaey <a.gmal@granadapride.com>
2025-01-01 11:04:13 +0200

    relations 2

--- app/Models/plans.php
+++ app/Models/plans.php
@@ -6,5 +6,41 @@ use Illuminate\Database\Eloquent\Model;
 
 class plans extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'duration_unit',
+        'description',
+        'price',
+        'discount_price',
+        'discount_type'
+    ];
+
+    public function features()
+    {
+        return $this->belongsToMany(features::class, 'plan_feature');
+    }
+
+}
+
+class features extends Model
+{
+    protected $fillable = [
+        'name',
+        'description',
+        'key',
+        'value'
+    ];
+
+    public function plans()
+    {
+        return $this->belongsToMany(plans::class, 'plan_feature');
+    }
+}
+
+class plan_feature extends Model
+{
+    protected $fillable = [
+        'plan_id',
+        'feature_id',
+    ];
 }
